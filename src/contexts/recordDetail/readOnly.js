7571242587a846bd89829d6d7528c61c0a508ee6
keyflight <olivier@andev.nl>
2019-01-27 11:46:42 +0100

    Remove import

--- src/contexts/recordDetail/readOnly.js
+++ src/contexts/recordDetail/readOnly.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import {css} from 'emotion'
-import nl2br from 'react-nl2br'
 import ReactMarkdown from 'react-markdown'
 
 export default class LongTextField extends React.Component {
